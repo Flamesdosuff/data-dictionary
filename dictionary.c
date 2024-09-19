4948b1443bee4c058482c1bfff441bd618b66a03
Flamesdosuff <144633371+Flamesdosuff@users.noreply.github.com>
2024-09-19 10:41:09 -0600

    dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -1,5 +1,11 @@
 #include "dictionary.h"
 
-void initializeDataDictionary() {
+void initializeDataDictionary(const char "diccionaryName") {
+    long mainHeader = -1;
     printf("Initializing Data Dictionary...\n");
+    FILE *dictionary = fopen("diccionario.dat","w+");
+    fwrite(&mainHeader,sizeof(mainHeader),1,dictionary);
+    long currentDir = ftell(dictionary);
+    
+    
 }
