f55dba9793bae0b8ea18c2c93f3169b3948e69a3
Flamesdosuff <144633371+Flamesdosuff@users.noreply.github.com>
2024-09-19 10:44:29 -0600

    dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -1,9 +1,11 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+#include <stdlib.h>
 #include <stdio.h>
 
 #define Empty_Pointer -1
-void initializeDataDictionary(const char "diccionaryName");
+
+bool initializeDataDictionary(const char "diccionaryName");
 
 #endif
