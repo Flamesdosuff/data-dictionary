d9632b8a5f6d0bc489e35481f49136d536e52536
Flamesdosuff <144633371+Flamesdosuff@users.noreply.github.com>
2024-10-13 23:04:04 +0000

    correccion

--- dictionary.c
+++ dictionary.c
@@ -1,27 +1,48 @@
 #include "dictionary.h"
 
-int initializeDataDictionary(const char diccionaryName) {
-    long mainHeader = Empty_Pointer;
-    int stringsToSave = 0;
-
-
-    printf("Initializing Data Dictionary...\n");
-
-    FILE *dictionary = fopen("diccionario.dat","w+");
-
-    fwrite(&mainHeader,sizeof(mainHeader),1,dictionary);
-
-    printf("How many strings would you like to save?");
-    scanf("%d",&stringsToSave);
-
-    for(int i = 0; i<stringsToSave; i++)
-    {
-        Node currentNode;
-        currentNode.next = Empty_Pointer;
-        printf("Enter a value for node #%d",i);
-        scanf("%s[^\n]",currentNode.value);
-        fwrite(&currentNode,sizeof(currentNode),1,dictionary);
+// Función para imprimir los nodos de la lista
+void printNodes(Node *head) {
+    Node *corre = head;
+    int cont = 1; 
+    printf("comprobacion de que se haya guardado los nodos a continuacion los nodos guardados: \n");
+    while (corre != NULL) {
+        printf("nodo #%d: %s\n", cont++, corre->value);
+        corre = corre->sig; 
     }
+}
+
+int initializeDataDictionary(const char* dictionaryName) {
+    Node *cab = NULL; // Variable para la cabeza de la lista
+    Node *fin = NULL; // Variable para el último nodo
+    printf("Initializing Data Dictionary...\n");
+    FILE *file = fopen(dictionaryName, "r");
+    if (!file) {
+        printf("Error to open file: %s\n", dictionaryName);
+        return -1;
+    }
+
+    char line[MAX_LINE_LENGTH];  
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = '\0';
+        Node *aux = malloc(sizeof(Node));
+        if (aux == NULL) {
+            printf("Error allocating memory for node\n");
+            fclose(file);
+            return -1;
+        }
+        aux->sig = NULL; 
+        strncpy(aux->value, line, sizeof(aux->value) - 1);
+        aux->value[sizeof(aux->value) - 1] = '\0'; 
+        if (cab == NULL) {
+            cab = aux; 
+        } else {
+            fin->sig = aux;
+        }
+        fin = aux; 
+    }
+
+    fclose(file);
+    printNodes(cab);
 
     return 1;
 }
--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include "main.h"
+#include <string.h>
 
 int main(int argc, char** argv) {
     char name[50];
 
-    printf("Enter a name of dictionary name");
-    fgets(name,50);   
-    initializeDataDictionary(name);    
+    printf("Enter the name of the .txt file to process (utilizar gramatica1.txt para el ejemplo): ");
+    fgets(name, 50, stdin);
+    name[strcspn(name, "\n")] = '\0';
+
+    if (strstr(name, ".txt") == NULL) {
+        printf("Error: The file must have a .txt extension.\n");
+        return -1;
+    }
+    initializeDataDictionary(name);
+
     return 0;
 }
+
+
+
+
+
+
